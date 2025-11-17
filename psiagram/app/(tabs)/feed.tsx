b5689da9bcec4ac3a0a358d50c8bbfe18254b002
Malgorzata_Ryll <gotkaryll@gmail.com>
2025-11-17 19:35:45 +0100

    protected routes

--- psiagram/app/(tabs)/feed.tsx
+++ psiagram/app/(tabs)/feed.tsx
@@ -1,9 +1,16 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
+import { useAuth } from "../../context/AuthContext";
 
-export default function FeedScreen() {
+export default function Feed() {
+  const { logout, user } = useAuth();
+
   return (
-    <View>
-      <Text> feed </Text>
+    <View style={{ padding: 20 }}>
+      <Text style={{ fontSize: 18, marginBottom: 20 }}>
+        Witaj, {user?.email}
+      </Text>
+
+      <Button title="test wylogowywania" onPress={logout} />
     </View>
   );
 }
--- psiagram/app/_layout.tsx
+++ psiagram/app/_layout.tsx
@@ -1,10 +1,25 @@
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
+import { AuthProvider, useAuth } from "../context/AuthContext";
+
+function AuthGate() {
+  const { isAuthenticated } = useAuth();
+
+  if (isAuthenticated) {
+    return <Redirect href="/(tabs)/feed" />;   //feed
+  }
+  
+  return <Redirect href="/(auth)/login" />;    //logowanie
+}
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(auth)" />
-      <Stack.Screen name="(tabs)" />
-    </Stack>
+    <AuthProvider>
+      <AuthGate />
+
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+    </AuthProvider>
   );
 }
